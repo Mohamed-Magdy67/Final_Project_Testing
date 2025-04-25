30cd254cfc4249b3d2736396e2657be063f76e23
Menna36200 <menna.mamdouh36200@gmail.com>
2025-04-25 19:55:35 +0300

    Update LoginTest.java

--- automation/src/test/java/Tests/LoginTest.java
+++ automation/src/test/java/Tests/LoginTest.java
@@ -1,22 +1,93 @@
 package Tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import Pages.LoginPage;
 
-public class LoginTest extends TestBase {
-    LoginPage loginPage ;
+public class LoginTest extends Test_Base {
+
+    private static final Logger log = LoggerFactory.getLogger(LoginTest.class);
+    LoginPage loginPage;
 
     @BeforeMethod
-    public void init () {
+    public void setup() {
         loginPage = new LoginPage(driver);
     }
 
     @Test
-    public void loginWithValidUserNameAndValidPassword () {
+    public void LoginWithValidUsernameAndValidPassword()  {
         loginPage.fillUserName("standard_user");
         loginPage.fillPassword("secret_sauce");
-        loginPage.clickLoginButton();
+        loginPage.clickOnLoginButton();
     }
 
+    @Test
+    public void loginWithoutData () {
+        loginPage.fillUserName("");
+        loginPage.fillPassword("");
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(loginPage.requiredErrorMessageUsernameActualResult(),
+                loginPage.getRequiredErrorUsernameTextExpectResult(),
+                "Epic sadface: Username is required\n");
+    }
+
+    @Test
+    public void loginWithJustValidPassword(){
+        loginPage.fillUserName("");
+        loginPage.fillPassword("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(loginPage.requiredErrorMessageUsernameActualResult(),
+                loginPage.getRequiredErrorUsernameTextExpectResult(),
+                "Epic sadface: Username is required\n");
+    }
+
+    @Test
+    public void loginWithJustValidUsername(){
+        loginPage.fillUserName("standard_user");
+        loginPage.fillPassword("");
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(loginPage.requiredErrorMessagePasswordActualResult(),
+                loginPage.getRequiredErrorPasswordTextExpectResult(),
+                "Epic sadface: Password is required\n");
+    }
+
+    @Test
+    public void loginWithInvalidUsernameAndValidPassword(){
+        loginPage.fillUserName("standard");
+        loginPage.fillPassword("secret_sauce");
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(loginPage.invalidCredentialActualResult(),
+                loginPage.getInvalidCredentialExpectedResult(),
+                "Epic sadface: Username and password do not match any user in this service\n");
+    }
+
+    @Test
+    public void loginWithValidUsernameAndInvalidPassword(){
+        loginPage.fillUserName("standard_user");
+        loginPage.fillPassword("secret");
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(loginPage.invalidCredentialActualResult(),
+                loginPage.getInvalidCredentialExpectedResult(),
+                "Epic sadface: Username and password do not match any user in this service\n");
+    }
+
+    @Test
+    public void loginWithInvalidUsernameAndInvalidPassword(){
+        loginPage.fillUserName("standard");
+        loginPage.fillPassword("secret");
+        loginPage.clickOnLoginButton();
+
+        Assert.assertEquals(loginPage.invalidCredentialActualResult(),
+                loginPage.getInvalidCredentialExpectedResult(),
+                "Epic sadface: Username and password do not match any user in this service\n");
+    }
 }
